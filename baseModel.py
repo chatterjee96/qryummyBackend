11390c89e6a980640963061739ccf757f650e035
chatterjee96 <119383049+chatterjee96@users.noreply.github.com>
2023-02-08 21:47:15 -0500

    First commit

---
+++ baseModel.py
@@ -0,0 +1,19 @@
+import peewee as pw
+
+# Variables for database connection
+
+# Dev db config
+endpoint = 'qryummyv2.cpda1jo1k8hj.us-east-1.rds.amazonaws.com'
+port = 3306
+dbuser = 'root'
+password = 'Password1234'
+database='qryummy'
+
+# Instantiating mysql database
+qryummy = pw.MySQLDatabase(database, host = endpoint, port = port, user = dbuser, passwd = password)
+
+class BaseModel(pw.Model):
+    """A base model that will use our MySQL database"""
+
+    class Meta:
+        database = qryummy
